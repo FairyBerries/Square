3d4ba50060a7b1f4b8341a447147a2cb901cff3a
Elena Tkachenko <elena_20.09@inbox.ru>
2022-01-24 23:18:18 +0300

    All tests

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +21,33 @@ class SQRServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldNoSquares() {
 
+        SQRService service = new SQRService();
+
+        int a = 500;
+        int b = 525;
+        int expected = 0;
+
+        int actual = service.squareCalc(a, b);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAllSquares() {
+
+        SQRService service = new SQRService();
+
+        int a = 100;
+        int b = 484;
+        int expected = 13;
+
+        int actual = service.squareCalc(a, b);
+
+        assertEquals(expected, actual);
+
+
+    }
 }
